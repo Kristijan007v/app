cba320183b0f00a2d62231382e0c38eca8a9cb99
Kristijan007v <84925309+Kristijan007v@users.noreply.github.com>
2022-04-15 23:27:05 +0200

    Additional accessibility improvments

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
       {/* Add whichever language you want here */}
       <Head>
         <title>Kristijan Vidović</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <body>
         <Main />
