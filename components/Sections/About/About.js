21369159ead448c3012c99987d5d99f02d851515
Kristijan007v <84925309+Kristijan007v@users.noreply.github.com>
2022-04-15 14:12:20 +0200

    Fixed overflow issue.

--- components/Sections/About/About.js
+++ components/Sections/About/About.js
@@ -30,55 +30,53 @@ export default function About() {
   };
 
   return (
-    <>
+    <div
+      id="about"
+      className="border__default flex flex-col space-y-6 lg:flex-row"
+    >
       <SectionHeading
         title="About"
         number={"01"}
         desciption={"Let me introduce myself."}
         align={"left"}
       />
-      <div
-        id="about"
-        className="border__default lg:flex-reverse flex flex-col space-y-6 lg:flex-row"
-      >
-        <div className="relative h-72 w-full">
-          <Image
-            priority
-            className="mask mask-squircle grayscale hover:grayscale-0"
-            alt={"Article Image"}
-            src={"/images/my_photo.webp"}
-            layout="fill"
-            objectFit="cover"
-          />
-        </div>
-        <div className="flex flex-col items-center justify-center space-y-4">
-          <div className="mockup-code bg-secondaryGray">
-            <pre data-prefix="$">
-              <code>print(date-of-birth)</code>
-            </pre>
-            <pre data-prefix=">" className="text-warning">
-              <code>07.03.2000.</code>
-            </pre>
-            <pre data-prefix=">" className="text-success">
-              <code>Done!</code>
-            </pre>
-            <pre data-prefix="$">
-              <code>print(education)</code>
-            </pre>
-            <pre data-prefix=">" className="text-warning">
-              <code>Highscool: Gimnazija Sesvete (2015-2019)</code>
-            </pre>
-            <pre data-prefix=">" className="text-success">
-              <code>Done!</code>
-            </pre>
-          </div>
-          <ButtonDefault
-            text={"Download My Resume"}
-            onclick={downloadFile}
-            ariaLabel={"Download my resume"}
-          />
-        </div>
+      <div className="relative h-72 w-full">
+        <Image
+          priority
+          className="mask mask-squircle grayscale hover:grayscale-0"
+          alt={"Article Image"}
+          src={"/images/my_photo.webp"}
+          layout="fill"
+          objectFit="cover"
+        />
       </div>
-    </>
+      <div>
+        <div className="mockup-code bg-secondaryGray">
+          <pre data-prefix="$">
+            <code>print(date-of-birth)</code>
+          </pre>
+          <pre data-prefix=">" className="text-warning">
+            <code>07.03.2000.</code>
+          </pre>
+          <pre data-prefix=">" className="text-success">
+            <code>Done!</code>
+          </pre>
+          <pre data-prefix="$">
+            <code>print(education)</code>
+          </pre>
+          <pre data-prefix=">" className="text-warning">
+            <code>Highscool: Gimnazija Sesvete (2015-2019)</code>
+          </pre>
+          <pre data-prefix=">" className="text-success">
+            <code>Done!</code>
+          </pre>
+        </div>
+        <ButtonDefault
+          text={"Download My Resume"}
+          onclick={downloadFile}
+          ariaLabel={"Download my resume"}
+        />
+      </div>
+    </div>
   );
 }
